9d35390ef4e0cd23cf5634d312627cdfa9a75ba7
Harsh Pratap Singh <74965077+HPSingh7146@users.noreply.github.com>
2024-09-22 02:11:18 +0530

    added logo img and did few changes

--- script.js
+++ script.js
@@ -10,17 +10,20 @@ window.addEventListener('scroll', () => {
   const zoomFactor = window.scrollY / (zoomElement.offsetHeight + window.innerHeight);
 
   // Calculate the opacity of the .detail and .pic elements
-  const opacity = 1 - (zoomFactor * 2);
+//   const opacity = 1 - (zoomFactor * 2);
 
-  // Set the opacity of the .detail and .pic elements
-  detailElements.forEach((element) => {
-    element.style.opacity = opacity;
-  });
+//   // Set the opacity of the .detail and .pic elements
+//   detailElements.forEach((element) => {
+//     element.style.opacity = opacity;
+//   });
 
-  picElements.forEach((element) => {
-    element.style.opacity = opacity;
-  });
+//   picElements.forEach((element) => {
+//     element.style.opacity = opacity;
+//   });
 
   // Set the background-size property of the #zoom element
-  zoomElement.style.backgroundSize = `calc(100% + ${zoomFactor * 30}%) auto`;
+  zoomElement.style.backgroundSize = `calc(100% + ${zoomFactor * 40}%) auto`;
 });
+
+
+
